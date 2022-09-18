0af7fc18e48107a6e20753c42d70c11f87b431b8
Khusan Fazliddinov <khusan1697@gmail.com>
2022-09-18 11:33:59 +0500

    Initial commit

---
+++ lib/page/color_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+import '../widget/state_widget.dart';
+
+class ColorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(MyApp.title),
+        ),
+        body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 64),
+          child: Center(
+            child: Container(
+              color: Colors.white,
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  buildColorButton(context, Colors.red),
+                  buildColorButton(context, Colors.orange),
+                  buildColorButton(context, Colors.greenAccent),
+                  buildColorButton(context, Colors.indigo),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+
+  Widget buildColorButton(BuildContext context, Color color) => Container(
+        margin: const EdgeInsets.symmetric(vertical: 8),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(primary: color, elevation: 0),
+          onPressed: () {
+            final provider = StateInheritedWidget.of(context);
+
+            provider.setColor(color);
+            Navigator.pop(context);
+          },
+          child: Container(height: 100),
+        ),
+      );
+}
